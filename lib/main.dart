9a2227c45915b26d49f44d6bb15df587e4ea379b
RahulB-era <rahulbera27april@gmail.com>
2024-04-20 21:04:03 +0530

    Revert "basic"
    This reverts commit 0dc35ad4dec21e8426158689ffb1188b538acf21.

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:gauge_indicator/gauge_indicator.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:dynamic_color/dynamic_color.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return DynamicColorBuilder(
-      builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: 'Audio Meter',
-          theme: ThemeData(
-            useMaterial3: true,
-            textTheme: GoogleFonts.poppinsTextTheme(),
-            primarySwatch: Colors.blue,
-            scaffoldBackgroundColor: Colors.white,
-            // scaffoldBackgroundColor: lightDynamic?.background ?? Colors.white,
-            // primaryColor: darkDynamic?.onPrimaryContainer ?? Colors.white,
-          ),
-          home: const MyHomePage(title: 'Audio Meter'),
-        );
-      },
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Audio Meter',
+      theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(),
+        primarySwatch: Colors.blue,
+
+      ),
+      home: const MyHomePage(title: 'Audio Meter'),
     );
   }
 }
@@ -47,87 +32,61 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int currentPageIndex = 0;
   @override
   Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.white,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
         title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.settings),
-              color: Colors.blue,
-            ),
+            IconButton(onPressed: (){}, icon: Icon(Icons.settings),color: Colors.blue,),
             Text(
               widget.title,
               style: GoogleFonts.poppins(
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold,
-                color: const Color.fromARGB(255, 0, 140, 255),
-              ),
+                  fontSize: 24.0, fontWeight: FontWeight.bold, color: const Color.fromARGB(255, 0, 140, 255)),
             ),
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.copy),
-              color: Colors.blue,
-            ),
-
-
+            IconButton(onPressed: (){}, icon: Icon(Icons.copy),color: Colors.blue,)
           ],
         ),
       ),
       body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
         child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            
             const Text(
               'You have pushed the button this many times:',
             ),
-            ElevatedButton.icon(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.play_arrow,
-                  color: Colors.blue,
-                ),
-                label: Text(
-                  'play',
-                  style: TextStyle(color: Colors.blue),
-                ))
           ],
         ),
       ),
-      bottomNavigationBar: NavigationBar(
-        onDestinationSelected: (int index) {
-          setState(() {
-            currentPageIndex = index;
-          });
-        },
-        indicatorColor: Colors.blue,
-        selectedIndex: currentPageIndex,
-        destinations: const <Widget>[
-          NavigationDestination(
-            selectedIcon: Icon(
-              Icons.multitrack_audio_rounded,
-              color: Colors.white,
-            ),
-            icon: Icon(Icons.multitrack_audio_rounded, color: Colors.grey),
-            label: 'Decibel Meter',
-          ),
-          NavigationDestination(
-            selectedIcon: Icon(
-              Icons.help_outline,
-              color: Colors.white,
-            ),
-            icon: Badge(child: Icon(Icons.help_outline, color: Colors.grey)),
-            label: 'help',
-          ),
-        ],
-        backgroundColor: Colors.white,
-        surfaceTintColor: Colors.white,
-      ),
+      bottomNavigationBar: BottomNavigationBar(items: [
+        
+      ]),
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
